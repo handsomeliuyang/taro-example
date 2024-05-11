2a18189886468311146bc13688877319ef4b9a9d
刘阳 <liuyang@58.com>
2024-05-11 10:09:14 +0800

    更新测试Demo

--- harmony-hybrid-vue-demo/src/app.ts
+++ harmony-hybrid-vue-demo/src/app.ts
@@ -2,11 +2,20 @@
 import { createApp } from 'vue'
 import './app.css'
 import Taro, { requirePlugin } from "@tarojs/taro";
+const Taro2 = require("@tarojs/taro")
 
 const App = createApp({
   onShow (options) {
+    console.log('ES Taro', Taro)
+    console.log('CommonJs Taro', Taro2)
+
+    console.log('ES getWindowInfo', Taro.getWindowInfo)
+    console.log('CommonJs getWindowInfo', Taro2.getWindowInfo)
+
+    console.log('ES getEnv', Taro.getEnv)
+    console.log('CommonJs getEnv', Taro2.getEnv)
+
     console.log('App onShow.', Taro.setGlobalDataPlugin)
-    console.log('Taro Object', Taro)
     console.log('requirePlugin', requirePlugin)
   },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
